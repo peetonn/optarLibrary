f2c95895c2443a5eb2c8830bea02eacecef96019
peetonn <gpeetonn@gmail.com>
2019-11-12 12:33:58 -0800

    move android to a subfolder

--- Android/jni/Android.mk
+++ Android/jni/Android.mk
@@ -3,13 +3,13 @@ include $(CLEAR_VARS)
 
 OPENCV_CAMERA_MODULES:=off
 OPENCV_INSTALL_MODULES:=on
-OPENCV_LIB_TYPE:=SHARED #STATIC
-include thirdparty/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+OPENCV_LIB_TYPE:=SHARED
+include ../thirdparty/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := optar
 LOCAL_CFLAGS    := -std=c++11
-LOCAL_SRC_FILES := ../source/optar.cpp
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../source
+LOCAL_SRC_FILES := ../../source/optar.cpp
+LOCAL_C_INCLUDES += ../../source
 #LOCAL_LDLIBS    += -llog -ldl -lGLESv2
 LOCAL_LDLIBS    += -llog -ldl -lGLESv3
 include $(BUILD_SHARED_LIBRARY)
---
+++ README.md
@@ -0,0 +1,34 @@
+# OPTAR Library
+
+This is a C++ implementation of OPTAR code that compiles into a library.
+Even though, it makes sense only for mobile devices, one can also build it for desktop platforms for debugging/development.
+
+## Prerequisites
+
+* OpenCV
+* [spdlog](https://github.com/gabime/spdlog)
+
+## Build prerequisites
+
+### Android
+
+### iOS
+
+### macOS
+
+```
+brew install opencv spdlog
+```
+
+### Ubuntu
+
+
+## Build library
+
+### Android
+
+### iOS
+
+### macOS
+
+### Ubuntu
