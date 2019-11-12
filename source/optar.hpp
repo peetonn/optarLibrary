d1f0cb69b615e4649276ea154b31cc14df4eb233
peetonn <gpeetonn@gmail.com>
2019-11-12 15:55:49 -0800

    compiling for macOS

--- source/optar.hpp
+++ source/optar.hpp
@@ -8,6 +8,8 @@
 #ifndef __optar_hpp__
 #define __optar_hpp__
 
+#include "config.hpp"
+
 namespace optar
 {
 
@@ -17,7 +19,7 @@ class OptarClient {
 public:
     OptarClient();
     ~OptarClient();
-    
+
     void processTexture(int w, int h, const void *rgbaData,
                         int &nKeypoints, int &nDescriptors);
 };
