696f5ed67307f0bf7e10dd3a96f2ba2c4358b9d4
peetonn <gpeetonn@gmail.com>
2019-11-12 12:04:50 -0800

    added unreal build .cs file

---
+++ optarLibrary.Build.cs
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class optarLibrary : ModuleRules
+{
+	public optarLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+	}
+}
